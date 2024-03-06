80072d1769aaac8fb026d2ed7f9d1100e8f0e799
Niventhanlenin07 <151647266+Niventhanlenin07@users.noreply.github.com>
2024-03-06 01:55:49 -0800

    Add files via upload

---
+++ problem1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner; 
+public class problem1{ 
+    public static void main(String[] args) { 
+        Scanner mynum=new Scanner(System.in); 
+
+        System.out.println("enter n:"); 
+
+        int n=mynum.nextInt(); 
+
+        System.out.println("MULTIPLICATION TABLE"); 
+
+        for (int i=1;i<=10;i++){ 
+
+            System.out.println(i+"*"+n+"="+i*n); 
+
+        } 
+
+        System.out.println("SUBTRACTION TABLE"); 
+
+        for (int j=1;j<=10;j++){ 
+
+            if (j>=n){ 
+
+                System.out.println(j+"-"+n+"="+(j-n)); 
+
+            }     
+
+        }     
+
+    } 
+
+}
+
