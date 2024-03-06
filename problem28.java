cab8747056020c8824fd97aabc698a014e7c6364
Niventhanlenin07 <151647266+Niventhanlenin07@users.noreply.github.com>
2024-03-06 19:38:48 +0530

    Add files via upload

---
+++ problem28.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class problem28 {
+    public static void main(String[] args){
+        Scanner mynum=new Scanner(System.in);
+        System.out.println("enter n :");
+        int n=mynum.nextInt();
+        if (n%2==0){
+            System.out.println("It\'s a even number");
+        }else{
+            System.out.println("It\'s a odd number");
+        }
+    }
+}
