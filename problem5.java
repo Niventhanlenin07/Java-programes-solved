b2bd7da084936bcfd19865f20d619efb21087a4b
Niventhanlenin07 <151647266+Niventhanlenin07@users.noreply.github.com>
2024-03-06 02:10:01 -0800

    Add files via upload

---
+++ problem5.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class problem5 {
+    public static void main(String[] args) { 
+
+        Scanner mynum=new Scanner(System.in); 
+
+        System.out.println("enter n :"); 
+
+        int n=mynum.nextInt(); 
+
+        for (int i=n;i>=1;i--){ 
+
+            for (int j=1;j<=i;j++){ 
+
+                System.out.printf("%C ",'A'-1+i); 
+
+            } 
+
+            System.out.printf("\n"); 
+
+        } 
+
+    } 
+
+} 
