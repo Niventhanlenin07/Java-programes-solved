6c9b3740c0a5aabedcddab70296a3ab4a1676354
Niventhanlenin <151647266+Niventhanlenin07@users.noreply.github.com>
2024-03-07 19:37:46 +0530

    Add files via upload

---
+++ problem49.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class problem49 {
+    public static void main(String[] args){
+        Scanner mynum=new Scanner(System.in);
+        System.out.print("enter n :");
+        int n=mynum.nextInt();
+        for (int i=n;i>0;i--){
+            for (int k=1;k<=n-i;k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
