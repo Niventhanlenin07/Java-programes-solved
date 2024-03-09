46e0cf9952499b02d3a34668afb6c57d0f332787
Niventhanlenin <151647266+Niventhanlenin07@users.noreply.github.com>
2024-03-09 19:54:06 +0530

    Add files via upload

---
+++ problem6.java
@@ -0,0 +1,20 @@
+class problem6 {
+    public static void main(String[] args){
+        for (int i=101;i<=120;i+=4){
+            System.out.println(i);
+        }
+        System.out.println(" ");
+        for (int j=102;j<=120;j+=4){
+            System.out.println(j);
+        }
+        System.out.println(" ");
+        for (int k=103;k<=120;k+=4){
+            System.out.println(k);
+        }
+        System.out.println(" ");
+        for (int l=104;l<=120;l+=4){
+            System.out.println(l);
+        }
+        System.out.println(" ");
+    }
+}
