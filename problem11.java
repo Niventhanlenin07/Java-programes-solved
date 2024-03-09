931cd54224d11d8c5da6310ee8efa825299c0215
Niventhanlenin <151647266+Niventhanlenin07@users.noreply.github.com>
2024-03-09 14:05:46 +0530

    Add files via upload

---
+++ problem11.java
@@ -0,0 +1,25 @@
+class problem11{
+    static void leftrotate(int arr[], int d, int n){
+        for (int i = 0; i < d; i++)
+            lrotate(arr, n);
+    }
+    static void lrotate(int arr[], int n){
+        int i, temp;
+        temp = arr[0];
+        for (i = 0; i < n - 1; i++)
+            arr[i] = arr[i + 1];
+        arr[i] = temp;
+    }
+    static void list(int arr[], int n){
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+    public static void main(String[] args){
+        problem43 rotate = new problem43();
+        int arr[] = { 1, 2, 3, 4, 5 };
+        int d=2;
+        int n=arr.length;
+        rotate.leftrotate(arr,d,n);
+        rotate.list(arr,n);
+    }
+}
