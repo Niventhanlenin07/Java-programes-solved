bea401438b2f77e1db6eb68cd2c80682467b8162
Niventhanlenin <151647266+Niventhanlenin07@users.noreply.github.com>
2024-03-10 11:25:34 +0530

    Add files via upload

---
+++ problem73.java
@@ -0,0 +1,34 @@
+import java.io.*;
+class problem73{
+    static void printMatrix(int M[][],int rowSize,int colSize){
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < colSize; j++)
+                System.out.print(M[i][j] + " ");
+            System.out.println();
+        }
+    }
+    static int[][] add(int A[][], int B[][] ,int size){
+        int i,j;
+        int C[][] = new int[size][size];
+ 
+        for (i = 0; i < size; i++)
+            for (j = 0; j < size; j++)
+                C[i][j] = A[i][j] + B[i][j];
+        return C;
+    }
+    public static void main(String[] args){
+        int size = 4;
+        int A[][] = { { 1, 1, 1, 1 },
+                      { 2, 2, 2, 2 },
+                      { 3, 3, 3, 3 },
+                      { 4, 4, 4, 4 } };
+ 
+        int B[][] = { { 1, 1, 1, 1 },
+                      { 2, 2, 2, 2 },
+                      { 3, 3, 3, 3 },
+                      { 4, 4, 4, 4 } };
+        int C[][] = add(A, B, size);              
+        System.out.println("\nResultant Matrix:");
+        printMatrix(C, size, size);
+    }
+}
