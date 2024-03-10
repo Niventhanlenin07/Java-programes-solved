d49f53c2d81c5d2bde53482a7f21ba6e63064df1
Niventhanlenin <151647266+Niventhanlenin07@users.noreply.github.com>
2024-03-10 10:40:09 +0530

    Add files via upload

---
+++ problem71.java
@@ -0,0 +1,9 @@
+import java.util.*; 
+class problem71{ 
+    public static void main(String args[]) { 
+        int arr[] = { 111, 13, 25, 9, 34, 1 }; 
+        int n = arr.length; 
+        Arrays.sort(arr); 
+        System.out.println("second smallest element is "+ arr[1]); 
+    } 
+} 
