64f60aa586f44cf7731406725f8e85164e428011
Niventhanlenin <151647266+Niventhanlenin07@users.noreply.github.com>
2024-03-11 09:52:47 +0530

    Add files via upload

---
+++ problem98.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class problem98{
+    public static void main(String[] args){
+        Scanner mychar=new Scanner(System.in);
+        System.out.println("enter the letter");
+        String x=mychar.nextLine();
+        String y=x.toUpperCase();
+        System.out.println(y);
+    }
+}
---
+++ problem99.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+class problem99{
+    public static void main(String[] args){
+        Scanner mychar=new Scanner(System.in);
+        System.out.println("enter the letter");
+        String x=mychar.nextLine();
+        String y=x.toLowerCase();
+        System.out.println(y);
+    }
+}
