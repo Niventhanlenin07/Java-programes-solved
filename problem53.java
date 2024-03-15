45cd05493f42cf782f50a30e29d9d52b63673687
Niventhanlenin <151647266+Niventhanlenin07@users.noreply.github.com>
2024-03-15 02:55:26 -0700

    Update problem53.java

--- problem53.java
+++ problem53.java
@@ -1,15 +1,44 @@
 class problem53{
-    public static void main(String[] args){
-        String letters="b3c6d5";
-        for (int i=0;i<letters.length();i++){
-            if(Character.isAlphabetic(letters.charAt(i))){
-                System.out.print(letters.charAt(i));
-            }else{
-                int x=Character.getNumericValue(letters.charAt(i));
-                for(int j=1;j<x;j++){
-                    System.out.print(letters.charAt(i-1));
+    public static void main(String[] args) {
+        String input = "a1b10";
+        String output = repeatCharacters(input);
+        System.out.println(output);
+    }
+
+    public static String repeatCharacters(String input) {
+        StringBuilder result = new StringBuilder();
+        char currentChar = ' ';
+        int count = 0;
+        boolean readingNumber = false;
+        StringBuilder numberBuilder = new StringBuilder();
+
+        for (char c : input.toCharArray()) {
+            if (Character.isAlphabetic(c)) {
+                if (readingNumber) {
+                    int repeatCount = Integer.parseInt(numberBuilder.toString());
+                    for (int i = 0; i < repeatCount; i++) {
+                        result.append(currentChar);
+                    }
+                    readingNumber = false;
+                    numberBuilder.setLength(0);
                 }
+                currentChar = c;
+            } else if (Character.isDigit(c)) {
+                if (!readingNumber && c == '0' && numberBuilder.length() == 0) {
+                    continue;
+                }
+                readingNumber = true;
+                numberBuilder.append(c);
             }
         }
+
+        if (readingNumber) {
+            int repeatCount = Integer.parseInt(numberBuilder.toString());
+            for (int i = 0; i < repeatCount; i++) {
+                result.append(currentChar);
+            }
+        }
+
+        return result.toString();
     }
-} 
+}
