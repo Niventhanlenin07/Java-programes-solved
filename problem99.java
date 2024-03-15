78b4b68cbd309c5e5a88a9076c9cd427dba2a2d6
Niventhanlenin <151647266+Niventhanlenin07@users.noreply.github.com>
2024-03-14 23:02:18 -0700

    Update problem99.java

--- problem99.java
+++ problem99.java
@@ -1,11 +1,16 @@
-
 import java.util.Scanner;
 class problem99{
     public static void main(String[] args){
         Scanner mychar=new Scanner(System.in);
         System.out.println("enter the letter");
         String x=mychar.nextLine();
-        String y=x.toLowerCase();
-        System.out.println(y);
+        char[] arr=x.toCharArray();
+        for (int i=0;i<arr.length;i++){
+            if (arr[i]>='a' && arr[i]<='z'){
+                arr[i]=(char)(arr[i]-32);
+            }
+        }
+        x=new String(arr);
+        System.out.println(x);
     }
 }
