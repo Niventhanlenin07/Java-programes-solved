ddd9d0d004332d206712463e9653cd6441d558f5
Niventhanlenin <151647266+Niventhanlenin07@users.noreply.github.com>
2024-03-15 14:26:51 +0530

    Update problem58.java

--- problem58.java
+++ problem58.java
@@ -1,27 +1,35 @@
-class probtlem58{
+import java.util.Scanner;
+class problem58{
     public static void main(String[] args) {
-        System.out.println("* * * * * * * *");
-        for (int i=1;i<=5;i++){
-            for (int j=4;j>0;j--){
-                System.out.print("*"+" ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input: ");
+        int num = scanner.nextInt();
+        printPattern(num);
+    }
+
+    public static void printPattern(int num) {
+        int totalRows = 2 * num - 1;
+        int midRow = totalRows / 2 + 1;
+        for (int i = 1; i <= totalRows; i++) {
+            int stars;
+            if (i <= midRow) {
+                stars = num - i + 1;
+            } else {
+                stars = i - midRow + 1;
             }
-            for (int j=1;j<=i;j++){
-                System.out.print("_");
-            }    
-            for (int j=4;j>0;j--){
-                System.out.print("*"+" ");
+            int dashes = num - stars;
+
+            for (int j = 0; j < stars; j++) {
+                System.out.print("*");
             }
-            System.out.println();
-        }
-        for (int i=1;i<=5;i++){
-            for (int j=4;j>0;j--){
-                System.out.print("*"+" ");
+            for (int j = 0; j < dashes; j++) {
+                System.out.print("-");
             }
-            for (int j=4;j>=i;j--){
-                System.out.print("_");
-            }    
-            for (int j=4;j>0;j--){
-                System.out.print("*"+" ");
+            for (int j = 0; j < dashes; j++) {
+                System.out.print("-");
+            }
+            for (int j = 0; j < stars; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
